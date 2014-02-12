681daab803878e9ed0ce139b07e9e4c4e61e379a
田村 章吾 <tamu.shu@gmail.com>
2014-02-12 12:46:38 +0900

    マカベンリベンジ資料追加

---
+++ 04-grunt-compass-ok/config.rb
@@ -0,0 +1,29 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "/"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+#SASS の書き出し方設定
+#output_style = :expanded or :nested or :compact or :compressed
+#sass_options = { :debug_info => true }
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+#絶対パスか相対パスかを指定
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+#CSS に SCSS での行番号を出力するかどうか
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
